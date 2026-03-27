50266430067f0685dd5ed240a622287732437bc9
hs-web-dev <hadr.slno@gmail.com>
2026-03-27 20:58:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ function addToSoundboard(id) {}
 function removeFromSoundboard(id) {}
 function playSound(id) {}
 
+async function importAudioFromYoutube(url) {
+  const response = await fetch("https://soundboard-backend-phi.vercel.app", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ url })
+  });
+
+  const blob = await response.blob(); // le MP3 reçu
+  const audioUrl = URL.createObjectURL(blob); // URL locale utilisable
+
+  addToLibrary(audioUrl); // tu vas créer cette fonction
+}
